d75524964bec31e8d593e97e75551ce0bff12c8d
adeputragea <adeputragea28@gmail.com>
2022-05-02 20:32:23 +0700

    listen server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const port = process.env.PORT;
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server runnig at port ${port}`);
+});
