f05ceee70de8a7b419180f3ae84567f162ee0dab
adeputragea <adeputragea28@gmail.com>
2022-05-06 22:42:01 +0700

    create UserSchema

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,1 +1,28 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
+const UserShema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  isAdmin: {
+    type: Boolean,
+  },
+});
+
+export default mongoose.model('UserModel', UserShema)
